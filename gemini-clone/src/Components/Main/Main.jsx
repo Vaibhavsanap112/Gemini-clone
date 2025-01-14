c71a5296fb10c14cd5bae48a445c8d305fc8ca60
Vaibhav <sanapvaibhav8767@gmail.com>
2025-01-14 20:32:00 +0530

    sidebar

---
+++ gemini-clone/src/Components/Main/Main.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <p>How Can I Help You Today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest a beutiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest a beutiful places to see on an upcoming road trip</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest a beutiful places to see on an upcoming road trip</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Suggest a beutiful places to see on an upcoming road trip</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+        <div className="main-botton">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Promt Here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display some false content please make sure dont trust it blindly 
+
+          </p>
+
+
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Main;
